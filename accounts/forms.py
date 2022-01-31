71bc90b2a13898c956fb04a89fd252cdaa3c8d97
engineer-km <evolvekm123@gmail.com>
2022-02-01 01:10:10 +0900

    65강 password 변경

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import (UserCreationForm, 
+                                       PasswordChangeForm as AuthPasswordChangeForm)
 from .models import User
 
 
@@ -30,3 +31,15 @@ class ProfileForm(forms.ModelForm):
         fields = ['avatar', 'first_name', 'last_name', 
                   'website_url', 'bio',
                   'phone_number', 'gender']
+        
+
+class PasswordChangeForm(AuthPasswordChangeForm):
+    def clean_new_password1(self):
+        old_password = self.cleaned_data.get('old_password')
+        new_passowrd1 = self.cleaned_data.get('new_password1')
+        if old_password and new_passowrd1:
+            if old_password == new_passowrd1:
+                raise forms.ValidationError(
+                    "새로운 비밀번호는 기존 비밀번호와 다르게 입력해주세요."
+                )
+        return new_passowrd1
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from . import views
 
 
 urlpatterns = [
     path('login/', views.login, name='login'),  # /accounts/login/ -> settings.LOGIN_URL
-    path('logout', views.logout, name='logout'),
+    path('logout/', views.logout, name='logout'),
+    path('password_change/', views.password_change, name='password_change'),
     path('signup/', views.signup, name='signup'),
     path('edit/', views.profile_edit, name='profile_edit'),
 ]
