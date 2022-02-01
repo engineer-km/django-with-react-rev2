0030206f8791e7a547a3362ed7a85967f9f99ac7
engineer-km <evolvekm123@gmail.com>
2022-02-01 18:25:42 +0900

    77강 자바스트립트 없이 포스팅 화면에 댓글 쓰기

--- instagram/views.py
+++ instagram/views.py
@@ -28,12 +28,14 @@ def index(request):
         .exclude(pk__in=request.user.following_set.all())[:3]
         # 전체 유저중 login 한 유저는 제외하고
         # 유저`가 팔로우한 유저`는 제외한다.
-    request.user.following_set.all()
+    
+    comment_form = CommentForm()
     
     return render(
         request, 'instagram/index.html', 
         {"post_list": post_list,
-         "suggested_user_list": suggested_user_list,}
+         "suggested_user_list": suggested_user_list,
+         'comment_form': comment_form, }
     )
 
 
@@ -59,9 +61,13 @@ def post_new(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    comment_form = CommentForm()
     return render(
         request, "instagram/post_detail.html", 
-        { 'post': post, }
+        { 
+            'post': post,
+            'comment_form': comment_form, 
+        }
     )
     
 
